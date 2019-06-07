c3ed476915d816fe143a36e2ba2bdfd6b70385d1
Viji Krishnaprasad <viji_krishnaprasad@mcafee.com>
2019-06-07 13:18:18 -0700

    Preserve the case of console config option/key names when reading

--- dxlconsole/_compat.py
+++ dxlconsole/_compat.py
@@ -9,6 +9,8 @@ except ImportError:
     from io import StringIO
 
 def read_file(config_parser, file_like_obj):
+    # Setting optionxform to str .This makes option/key names case sensitive
+    config_parser.optionxform = str
     return config_parser.read_file(file_like_obj) \
         if hasattr(config_parser, "read_file") else \
         config_parser.readfp(file_like_obj)
